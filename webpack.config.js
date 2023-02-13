0b55fd23d821f1cbc3a31cb42b91c3014ec04297
Stef Altavista <stefanomsc@hotmail.it>
2023-02-13 16:16:23 +0100

    React app initialized

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HTMLwebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: ["./client/src/start.js"],
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js",
+    },
+    plugins: [
+        new HTMLwebpackPlugin({
+            template: "./client/src/index.html",
+            favicon: "./public/favicon.ico",
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, "public"),
+        proxy: {
+            "/": {
+                target: "http://localhost:5001",
+            },
+        },
+        port: "5000",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
