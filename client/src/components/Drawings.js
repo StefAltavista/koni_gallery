5577af0b1054e03fd1f0e7b60932bee9571dfad3
Stef Altavista <stefanomsc@hotmail.it>
2023-02-27 18:42:29 +0100

    drawing carousel implemented

--- client/src/components/Drawings.js
+++ client/src/components/Drawings.js
@@ -1,15 +1,44 @@
-import React from "react";
-// import useWindowDimensions from "../hooks/useWindowDimension";
+import React, { createRef, useEffect, useRef, useState } from "react";
 import "../../css/drawings.css";
 
 export default function Drawings({ files }) {
-    // const { width, height, scroll } = useWindowDimensions();
-    console.log(scroll);
+    const [inView, setInView] = useState(0);
+    const gallery = useRef();
+    const drawing = [];
+
+    for (let i in files) {
+        drawing[i] = useRef();
+    }
+
+    useEffect(() => {
+        drawing[inView]
+            ? drawing[inView].current.scrollIntoView({ behavior: "smooth" })
+            : null;
+    }, [inView]);
 
     return (
         <div id="drawings">
-            {files &&
-                files.map((x, i) => <img src={x} key={i} loading="lazy" />)}
+            <img
+                src="arrowL.png"
+                className="arrow"
+                onClick={() =>
+                    setInView(Math.floor(gallery.current.scrollLeft / 800) - 1)
+                }
+            />
+            <div ref={gallery} id="drawingsGallery">
+                {files.map((url, idx) => (
+                    <div key={idx} ref={drawing[idx]}>
+                        <img src={url} />
+                    </div>
+                ))}
+            </div>
+            <img
+                src="arrowR.png"
+                className="arrow"
+                onClick={() =>
+                    setInView(Math.floor(gallery.current.scrollLeft / 800) + 1)
+                }
+            />
         </div>
     );
 }
